5ccfa97b19166bb2a9a6f3d1dd2b7ec94b0f76e7
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 00:36:07 +0900

    修正

--- javaScript/loginCheck.js
+++ javaScript/loginCheck.js
@@ -1,30 +1,11 @@
 var req = new XMLHttpRequest();
-req.onreadystatechange = function() {
-    switch ( req.readyState ) {
-        case 0:
-            // 未初期化状態.
-            console.log( 'uninitialized!' );
-            break;
-        case 1: // データ送信中.
-            console.log( 'loading...' );
-            break;
-        case 2: // 応答待ち.
-            console.log( 'loaded.' );
-            break;
-        case 3: // データ受信中.
-            console.log('interactive... '+req.responseText.length+' bytes.' );
-            break;
-        case 4: // データ受信完了.
-            if( req.status == 200 || req.status == 304 ) {
-			}
-		}
-	}
+
 //idとpass送信
 function execPost() {
 	var userIdValue = document.getElementById('userId').value;
 	var userPassValue = document.getElementById('userPass').value;
 	var param = "user_id="+userIdValue+"user_pass"+userPassValue;
-	req.open('POST', '../php/idCheck.php', true);
+	req.open('POST', 'http://ec2-54-245-28-75.us-west-2.compute.amazonaws.com/server_training/php/idCheck.php', true);
 	req.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
 	req.send(param);
 }
