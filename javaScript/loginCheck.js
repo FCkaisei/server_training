b6d9bcdaacbbb3637a17e85119f24f1b800d71bb
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 01:58:40 +0900

    コメントアウト削除

--- javaScript/loginCheck.js
+++ javaScript/loginCheck.js
@@ -1,11 +1,11 @@
-var req = new XMLHttpRequest();
-//idとpass送信
+var req2 = new XMLHttpRequest();
+
 function execPost() {
 	var userIdValue = document.getElementById('userId').value;
 	var userPassValue = document.getElementById('userPass').value;
 	var userData = [userIdValue,userPassValue];
 	var param = "user_data="+userData;
-	req.open('POST', '../php/idCheck.php', true);
-	req.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
-	req.send(param);
+	req2.open('POST', '../php/idCheck.php', true);
+	req2.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+	req2.send(param);
 }
