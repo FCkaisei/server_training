05348d1bcf95395f07c0ce9819c75d40691872af
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-06 18:16:55 +0900

    ちょっと編集

--- php/tweet_get.php
+++ php/tweet_get.php
@@ -10,15 +10,14 @@ $stmt = $pdo->prepare("SELECT * FROM tweets WHERE user_id = ? ORDER BY id DESC")
 $stmt->bindValue(1, $tmpSess, PDO::PARAM_STR);
 $stmt->execute();
 $result = $stmt->fetchAll();
-$resultJson = json_encode($result);
+//$resultJson = json_encode($result);
 $array = array();
 foreach($result as $data){
 	$tmpArray = array();
-
 	$tmpArray['id'] = $data[0];
 	array_push($array, $tmpArray);
 }
-
-echo $array;
+$resultJson = json_encode($array);
+echo $resultJson;
 
  ?>
