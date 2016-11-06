363081da5b4a9fa5d55ed9b73db9607a101a8e63
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 03:26:36 +0900

    絶対動く

--- php/checkId.php
+++ php/checkId.php
@@ -2,24 +2,18 @@
 $user_data = $_POST['user_data'];
 $user_id = $user_data;
 $user_pass = $user_data;
-if(($user_id == "") || ($user_pass == "")) {
-
+if(($user_id == "") || ($user_pass == "")){
 }
 else{
-
 	require_once("../baseDB/connect_db.php");
 	$dsn = 'mysql:dbname=UserLoginDB;host=localhost';
 	$user = "root";
 	$password = "";
 	try{
-
 		$pdo = new PDO($dsn, $user, $password);
-
 		$pdo->query('SET NAMES utf8');
 	}
 	catch(PDOException $e){
-
-
 		die();
 	}
 	$sql = "select * from members";
@@ -34,19 +28,16 @@ else{
 	}
 	if(!isset($dbPassword)){
 		error(2);
-	}else {
-	if($dbPassword != $user_pass){
-
 	}
 	else {
-
-		session_start();
-		$_SESSION['user'] = $user_id;
-	
-		header('Location: ../html/tweet_main.html');
-		exit();
+		if($dbPassword != $user_pass){
+		}
+		else {
+			session_start();
+			$_SESSION['user'] = $user_id;
+			header('Location: ../html/tweet_main.html');
+			exit();
+		}
 	}
-}
-
 }
 ?>
