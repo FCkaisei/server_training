0ce8c40036a4a42c174a6efe2cd643ac3d53f921
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 02:03:56 +0900

    URL変更

--- php/idCheck.php
+++ php/idCheck.php
@@ -4,7 +4,6 @@ $user_data = $POST["user_data"]
 $user_id = $user_data[0];
 $user_pass = $user_data[1];
 
-
 if(($user_id == "") || ($user_pass == "")) {
 }
 else{
@@ -25,14 +24,12 @@ else{
 	$sth = $pdo->prepare($sql);
 	$sth->execute();
 	$sqlResult = $sth->fetchAll();
-	//フォームから取得したUSERIDとデータベース内のUSERIDが一致したらデータベースのPASSWORDを変数に格納
 	foreach($sqlResult as $data){
-		if($data['userid'] == $user_id) {  //フォームから取得したUSERIDとデータベースのUSERIDが一致
+		if($data['userid'] == $user_id) {
 			$dbPassword = $data['password'];
 			break;
 		}
 	}
-	//$dbPasswordという変数に値が格納されていない場合→formUserIdとデータベースのIDが不一致
 	if(!isset($dbPassword)){
 	  error(2);
 	}else {
@@ -42,9 +39,8 @@ else{
 	  } else {
 		//セッション作成
 		session_start();
-		//セッション変数を作成→セッション変数に　$formUserID を登録
 		$_SESSION['user'] = $user_id;
-		header("Location:../html/tweet_main.html");
+		header('location:./html/tweet_main.html');
 		exit();
 	}
 }
