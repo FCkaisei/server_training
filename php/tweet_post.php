f033f9b6416a44b09c5f1c627ec612406a9f4536
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 01:49:21 +0900

    loginUser設定

--- php/tweet_post.php
+++ php/tweet_post.php
@@ -1,12 +1,18 @@
 <?php
-$tweet_text = $_POST['tweet_text'];
 
-session_start();
+#$loginUser = $_SESSION['loginUser']
 $loginUser = "aaaaaa";
-#$_SESSION['user'];
 
-#データ・ベースに接続
+// メールアドレス取得
+$tweet_text = $_POST["tweet_text"];
+//エラーメッセージ配列
+$error = array();
+//データ・ベースに接続
 require_once("../baseDB/connect_db.php");
+
+if($tweet_text == ""){
+	array_push($error, "ツイートを入力しよう！");
+}
 else{
 	$now = date('Y-m-d H:i:s');
 	$stmt = $pdo->prepare("INSERT INTO tweets(user_id,tweet_text,time)VALUES(:user_id, :tweet_text, :timer)");
@@ -14,5 +20,11 @@ else{
 	$stmt->bindValue(':tweet_text', $tweet_text, PDO::PARAM_STR);
 	$stmt->bindValue(':timer', $now, PDO::PARAM_STR);
 	$stmt->execute();
+	if($stmt == false){
+		array_push($error, "tweetが登録できませんでした");
+	}
+	else{
+		console.Log("OK");
+	}
 }
 ?>
