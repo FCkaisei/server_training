c399dd1c934ddcbd7257ebcb64d1fd3a703f0b35
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-06 16:44:08 +0900

    getTweetを追加＆テスト

--- php/tweet_get.php
+++ php/tweet_get.php
@@ -9,25 +9,7 @@ $stmt = $pdo->prepare("SELECT * FROM tweets WHERE user_id = ? ORDER BY id DESC")
 $stmt->bindValue(1, $tmpSess, PDO::PARAM_STR);
 $stmt->execute();
 $result = $stmt->fetchAll();
-?>
-<table>
-        <caption>ツイート一覧</caption>
-<?php
-foreach($result as $data){
-    echo "<tr>";
-    echo "<td>" . $data[0];
-    echo "</td>";
-    echo "<td>" . $data[1];
-    echo "</td>";
-    echo "<td>" . $data[3];
-    echo "</td>";
-    echo "</tr>";
-    echo "<tr>";
-    echo "<td>ツイート内容:" . $data[2];
-    echo "</td>";
-    echo "</tr>";
-}
-
-//$query = "select * from tweets where user_id = '".$tmpSess."'";
+$resultJson = json_encode($result);
+echo $resultJson;
 
  ?>
