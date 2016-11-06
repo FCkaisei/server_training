cf6008d63c79b8c419bac97acdabf2aa64322e51
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-06 21:00:27 +0900

    修正

--- php/tweet_get.php
+++ php/tweet_get.php
@@ -1,19 +1,16 @@
 <?php
+echo($_SESSION['user']);
+
 #$tmpSess = $_SESSION['user'];
 $tmpSess = "aaaaaa";
 require_once("../baseDB/connect_db.php");
+//$sql = "SELECT * FROM tweets WHERE user_id = :id ORDER BY id DESC";
 $stmt = $pdo->prepare("SELECT * FROM tweets WHERE user_id = ? ORDER BY id DESC");
+
 $stmt->bindValue(1, $tmpSess, PDO::PARAM_STR);
 $stmt->execute();
 $result = $stmt->fetchAll();
-//$resultJson = json_encode($result);
-$array = array();
-foreach($result as $data){
-	$tmpArray = array();
-	$tmpArray['id'] = $data[0];
-	array_push($array, $tmpArray);
-}
-$resultJson = json_encode($array);
+$resultJson = json_encode($result);
 echo $resultJson;
 
  ?>
