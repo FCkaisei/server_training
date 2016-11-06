702e74c4ad90ca76c42ca0ce49823c15a3de1ef8
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 00:32:21 +0900

    修正

--- javaScript/loginCheck.js
+++ javaScript/loginCheck.js
@@ -1,4 +1,24 @@
 var req = new XMLHttpRequest();
+req.onreadystatechange = function() {
+    switch ( req.readyState ) {
+        case 0:
+            // 未初期化状態.
+            console.log( 'uninitialized!' );
+            break;
+        case 1: // データ送信中.
+            console.log( 'loading...' );
+            break;
+        case 2: // 応答待ち.
+            console.log( 'loaded.' );
+            break;
+        case 3: // データ受信中.
+            console.log('interactive... '+req.responseText.length+' bytes.' );
+            break;
+        case 4: // データ受信完了.
+            if( req.status == 200 || req.status == 304 ) {
+			}
+		}
+	}
 //idとpass送信
 function execPost() {
 	var userIdValue = document.getElementById('userId').value;
