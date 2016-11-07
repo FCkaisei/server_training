0f291144ae83648890a045fbc53c9ff5197515bb
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 17:20:18 +0900

    search

--- php/user_search.php
+++ php/user_search.php
@@ -1,8 +1,8 @@
 <?php
 	$othersId = $_POST["others_id"];
 	require_once("../baseDB/connect_db.php");
-	$stmt = $pdo->prepare("SELECT * FROM members WHERE userid = %?% ORDER BY userid DESC");
-	$stmt->bindValue(1, $othersId, PDO::PARAM_STR);
+	$stmt = $pdo->prepare("SELECT * FROM members WHERE userid = ? ORDER BY userid DESC");
+	$stmt->bindValue(1, %+$othersId+%, PDO::PARAM_STR);
 	$stmt->execute();
 	$result = $stmt->fetchAll();
 	$resultJson = json_encode($result);
