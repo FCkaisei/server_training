be20b41bdc2826180d5a8fcabc0c2b9d55a5c5a5
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-08 00:33:24 +0900

    フォローしているユーザのツイート取得

--- php/tweet_get.php
+++ php/tweet_get.php
@@ -5,7 +5,7 @@
 	//memberテーブル内のuseridが「kaisei」でありながらuseridが右の式の中の値ではない物
 	//'SELECT * FROM members WHERE userid LIKE ? AND userid NOT IN (SELECT follow_id FROM follows WHERE user_id LIKE ?)'
 	//SELECT * FROM tweets WHERE user_id IN (SELECT user_id FROM follows WHERE follow_id LIKE "kaisei");
-	$stmt = $pdo->prepare("SELECT * FROM tweets WHERE user_id IN (SELECT user_id FROM follows WHERE follow_id LIKE ?)");
+	$stmt = $pdo->prepare("SELECT * FROM tweets WHERE user_id IN (SELECT user_id FROM follows WHERE follow_id LIKE ?) ORDER BY time DESC");
 	$stmt->bindValue(1, $tmpSess, PDO::PARAM_STR);
 	$stmt->execute();
 	$result = $stmt->fetchAll();
