537fb49c119f95de889b4ce1c0bf7e2e588ce657
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 11:19:36 +0900

    email登録機能修正開始

--- register/registerManager.php
+++ register/registerManager.php
@@ -1,47 +1,39 @@
 <?php
-require_once('../chromephp/ChromePhp.php');
-ChromePhp::Log('include');
-
 /* 登録処理（終了を知らせる値）によって読み込むファイルを変える */
 $mode = $_POST["mode"];
-ChromePhp::Log($mode);
-
 if($_GET['pre_userid'] !="") {
   $mode = "regist_form";
 }
 
 switch($mode) {
-  // メールアドレスの登録と仮ID送信
-  case"email_regist":
-  $module = "email_regist.php";
-  break;
-  //会員登録フォーム
-  case"regist_form":
-  $module = "regist_form.php";
-  break;
-  //登録内容確認
-  case"regist_confirm":
-  $module = "regist_confirm.php";
-  break;
-  //会員登録
-  case"user_regist":
-  $module = "user_regist.php";
-  break;
-  //メールアドレス登録（初期画面）
-  default:
-  $module = "email_form.php";
-  break;
+	
+  	case"email_regist":
+		header('Location: ../php/email_regist.php');
+    	exit;
+	break;
+	  //会員登録フォーム画面
+	  case"regist_form":
+		  header('Location: ../php/regist_form.php');
+		  exit;
+	  break;
+
+	  //登録内容確認画面
+	  case"regist_confirm":
+	  	header('Location: ../php/regist_confirm.php');
+	  	exit;
+	  break;
+
+	  //会員登録画面
+	  case"user_regist":
+	  $module = "user_regist.php";
+	  header('Location: ../html/user_regist.php');
+	  exit;
+	  break;
+
+	  //メールアドレス登録（初期画面）
+	  default:
+	  header('Location: ../html/email_form.html');
+	  exit;
+	  break;
 }
 ?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>会員登録フォーム</title>
-</head>
-<body>
-<?php
-  //別のPHPファイルを読み込む（Singleton）
-  require_once($module);
-?>
-</body>
-</html>
