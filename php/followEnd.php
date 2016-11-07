84dba8e855f967e2542ff941c6fd549b00f93461
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 22:30:22 +0900

    多重検索

--- php/followEnd.php
+++ php/followEnd.php
@@ -1,14 +1,14 @@
 <?php
-$user_id = $_POST["user_id"];
+	$user_id = $_POST["user_id"];
 
-require_once("../baseDB/connect_db.php");
-$stmt = $pdo->prepare("INSERT INTO follows(follow_id,user_id)VALUES(:follow_id,:user_id)");
-$stmt->bindValue(':user_id',$_SESSION['loginUser'],PDO::PARAM_STR);
-$stmt->bindValue(':follow_id', $user_id,PDO::PARAM_STR);
-$stmt->execute();
-if($stmt == false){
-	array_push($error, "tweetが登録できませんでした");
-}
-else{
-}
- ?>
+	require_once("../baseDB/connect_db.php");
+	$stmt = $pdo->prepare("INSERT INTO follows(follow_id,user_id)VALUES(:follow_id,:user_id)");
+	$stmt->bindValue(':user_id',$_SESSION['loginUser'],PDO::PARAM_STR);
+	$stmt->bindValue(':follow_id', $user_id,PDO::PARAM_STR);
+	$stmt->execute();
+	if($stmt == false){
+		array_push($error, "tweetが登録できませんでした");
+	}
+	else{
+	}
+?>
--- php/user_search.php
+++ php/user_search.php
@@ -1,8 +1,15 @@
 <?php
+	session_start();
+	$tmpSess = $_SESSION['user'];
 	$othersId = $_POST["others_id"];
 	require_once("../baseDB/connect_db.php");
+	//SELECT * FROM members WHERE userid LIKE "%kaisei%" AND userid NOT IN (SELECT follow_id FROM follows WHERE user_id="aaaaaa");
+	$stmt = $pdo->prepare('SELECT * FROM members WHERE userid LIKE "%"?"%" AND userid NOT IN (SELECT follow_id FROM follows WHERE user_id LIKE ?)');
+
 	$stmt = $pdo->prepare("SELECT * FROM members WHERE userid LIKE ? ORDER BY userid DESC");
 	$stmt->bindValue(1, $othersId, PDO::PARAM_STR);
+	$stmt->bindValue(2, $tmpSess, PDO::PARAM_STR);
+
 	$stmt->execute();
 	$result = $stmt->fetchAll();
 	$resultJson = json_encode($result);
