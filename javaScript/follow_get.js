7b309d13826a37a82aef5db56c7b63743e7d200a
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 18:52:22 +0900

    フォローボタン修正

--- javaScript/follow_get.js
+++ javaScript/follow_get.js
@@ -23,7 +23,8 @@ req.onreadystatechange = function() {
 				for(var i = 0; i < jsonObject.length; i++){
 					var element = document.createElement('tr');
 					var buttonElement = document.createElement("button");
-					buttonElement.onclick = function(followOther(jsonObject[i]["userid"]));
+
+					buttonElement.onclick = followOther(jsonObject[i]["userid"]);
 					element.appendChild(buttonElement);
 					tweetBox.appendChild(element);
 				}
