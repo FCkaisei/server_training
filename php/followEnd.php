98a9a318f7c3bf95476d563911e32c77504c3d6e
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 22:56:25 +0900

    SESS追加

--- php/followEnd.php
+++ php/followEnd.php
@@ -1,9 +1,10 @@
 <?php
+	session_start();
+	$tmpSess = $_SESSION['user'];
 	$user_id = $_POST["user_id"];
-
 	require_once("../baseDB/connect_db.php");
 	$stmt = $pdo->prepare("INSERT INTO follows(follow_id,user_id)VALUES(:follow_id,:user_id)");
-	$stmt->bindValue(':user_id',$_SESSION['loginUser'],PDO::PARAM_STR);
+	$stmt->bindValue(':user_id',$tmpSess,PDO::PARAM_STR);
 	$stmt->bindValue(':follow_id', $user_id,PDO::PARAM_STR);
 	$stmt->execute();
 	if($stmt == false){
