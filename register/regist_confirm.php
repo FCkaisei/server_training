4f625204df6e627d86266fe41ce7c336a37e68ff
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 12:10:13 +0900

    エラー処理

--- register/regist_confirm.php
+++ register/regist_confirm.php
@@ -30,6 +30,26 @@ if(strlen($input_password) < 6 || strlen($input_password) > 16) {
 <?php
 /* エラー 入力フォーム表示 $error */
 if(count($error) > 0) {
+
+	if(count($error)){
+		foreach($error as $value){
+			?>
+			<table>
+				<caption>メールアドレス登録エラー</caption>
+				<tr>
+					<td class = "item">Error:</td>
+					<td>
+						<?php
+						print $value;
+						?>
+					</td>
+				</tr>
+			</table>
+			<?php
+		}
+	}
+
+
   //require_once("regist_form.php");
 ?>
 </div>
