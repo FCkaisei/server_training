3d1836e4d9f2e66e878a6bdbb1626f9d15e0f48d
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 23:10:20 +0900

    自動更新

--- javaScript/follow_get.js
+++ javaScript/follow_get.js
@@ -17,6 +17,7 @@ req.onreadystatechange = function() {
             if( req.status == 200 || req.status == 304 ) {
 				var jsonString = req.responseText;
 				if(jsonString == null || jsonString == "" || jsonString == undefined ){
+					execPost();
 					return;
 				}
 				var jsonObject = JSON.parse(jsonString);
--- php/tweet_post.php
+++ php/tweet_post.php
@@ -1,26 +1,24 @@
 <?php
-#$loginUser = $_SESSION['loginUser']
-$loginUser = "aaaaaa";
-$tweet_text = $_POST["tweet_text"];
-$error = array();
-require_once("../baseDB/connect_db.php");
+	#$loginUser = $_SESSION['loginUser']
+	$loginUser = "aaaaaa";
+	$tweet_text = $_POST["tweet_text"];
+	$error = array();
+	require_once("../baseDB/connect_db.php");
 
-if($tweet_text == ""){
-	array_push($error, "ツイートを入力しよう！");
-}
-
-else{
-	$now = date('Y-m-d H:i:s');
-	$stmt = $pdo->prepare("INSERT INTO tweets(user_id,tweet_text,time)VALUES(:user_id, :tweet_text, :timer)");
-	$stmt->bindValue(':user_id',$loginUser, PDO::PARAM_STR);
-	$stmt->bindValue(':tweet_text', $tweet_text, PDO::PARAM_STR);
-	$stmt->bindValue(':timer', $now, PDO::PARAM_STR);
-	$stmt->execute();
-	if($stmt == false){
-		array_push($error, "tweetが登録できませんでした");
+	if($tweet_text == ""){
+		array_push($error, "ツイートを入力しよう！");
 	}
+
 	else{
-		console.Log("OK");
+		$now = date('Y-m-d H:i:s');
+		$stmt = $pdo->prepare("INSERT INTO tweets(user_id,tweet_text,time)VALUES(:user_id, :tweet_text, :timer)");
+		$stmt->bindValue(':user_id',$loginUser, PDO::PARAM_STR);
+		$stmt->bindValue(':tweet_text', $tweet_text, PDO::PARAM_STR);
+		$stmt->bindValue(':timer', $now, PDO::PARAM_STR);
+		$stmt->execute();
+		if($stmt == false){
+		}
+		else{
+		}
 	}
-}
 ?>
