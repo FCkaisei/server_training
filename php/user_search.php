8fb5f38534c1fad5e98fa24bbc4cea9ef199141f
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-07 16:10:07 +0900

    ユーザー検索機能追加

--- php/user_search.php
+++ php/user_search.php
@@ -3,8 +3,8 @@
 	$userId = $_SESSION['user'];
 	$tweet_text = $_POST["others_id"];
 	require_once("../baseDB/connect_db.php");
-	$stmt = $pdo->prepare("SELECT * FROM members WHERE user_id = ? ORDER BY id DESC");
-	$stmt->bindValue(1, $userId, PDO::PARAM_STR);
+	$stmt = $pdo->prepare("SELECT * FROM members WHERE userid = ? ORDER BY id DESC");
+	$stmt->bindValue(1, $tweet_text, PDO::PARAM_STR);
 	$stmt->execute();
 	$result = $stmt->fetchAll();
 	$resultJson = json_encode($result);
