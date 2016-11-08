cbfed7b0b10b38c850c0c311578f9d48e3035a1d
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-08 13:30:59 +0900

    フォローユーザー一覧表示

---
+++ javaScript/follow_user_get.js
@@ -0,0 +1,58 @@
+var req = new XMLHttpRequest();
+req.onreadystatechange = function() {
+    switch ( req.readyState ) {
+        case 0:
+            console.log( 'uninitialized!' );
+            break;
+        case 1:
+            console.log( 'loading...' );
+            break;
+        case 2:
+            console.log( 'loaded.' );
+            break;
+        case 3:
+            console.log('interactive... '+req.responseText.length+' bytes.' );
+            break;
+        case 4:
+            if( req.status == 200 || req.status == 304 ) {
+				var jsonString = req.responseText;
+				if(jsonString == null || jsonString == "" || jsonString == undefined ){
+					execPost();
+					return;
+				}
+				var jsonObject = JSON.parse(jsonString);
+				console.log(jsonObject);
+				var tweetBox = document.getElementById("user");
+				tweetBox.innerHTML = "";
+				for(var i = 0; i < jsonObject.length; i++){
+					var element = document.createElement('tr');
+					var tdElement = document.createElement("td");
+					var buttonElement = document.createElement("button");
+					var buttonText = document.createTextNode("アンフォロー");
+					buttonElement.appendChild(buttonText);
+					var td2Element = document.createElement("td");
+					var userName = document.createElement("div");
+					var newtext = document.createTextNode(jsonObject[i]["follow_id"]);
+					userName.appendChild(newtext);
+
+					buttonElement.onclick = followOther;
+					buttonElement.setAttribute("data-userid",jsonObject[i]["follow_id"]);
+
+					element.appendChild(tdElement);
+					tdElement.appendChild(buttonElement);
+					element.appendChild(td2Element);
+					td2Element.appendChild(userName);
+					tweetBox.appendChild(element);
+				}
+            } else {
+                console.log( 'Failed. HttpStatus: '+req.statusText );
+            }
+            break;
+		}
+	}
+
+function getFollowUser(){
+	req.open('POST', '../php/follow_get.php', true);
+	req.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+	req.send(null);
+}
