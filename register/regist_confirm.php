be58d58de053b216fcd82a091aa29dd24b7d852f
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-08 14:40:05 +0900

    複数箇所修正

--- register/regist_confirm.php
+++ register/regist_confirm.php
@@ -3,18 +3,6 @@ $error = array();
 $formList = array('mode','pre_userid','input_userid','input_password','input_name','input_email');
 $requireList = array('mode','input_userid','input_password','input_name');
 
-foreach($formList as $value) {
-  $$value = $_POST[$value];
-}
-
-/* 必須項目入力チェック */
-foreach($requireList as $value) {
-	if($$value == "") {
-		array_push($error,"入力データが足りません");
-		break;
-	}
-}
-
 /* パスワードチェック */
 if(strlen($input_password) < 6 || strlen($input_password) > 16) {
   array_push($error,"pass 6-16");
