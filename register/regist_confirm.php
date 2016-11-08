aeefafdeeee6a4ebe96f813cbbf1ffb84c01c50c
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-08 16:44:31 +0900

    コード整理

--- register/regist_confirm.php
+++ register/regist_confirm.php
@@ -2,3 +2,11 @@
 	ini_set("display_errors", On);
 	error_reporting(E_ALL);
 ?>
+
+<?php
+	$error = array();
+	$userId = $_POST['input_userid'];
+	$userPass = $_POST['input_password'];
+	$userEmail = $_POST['input_email'];
+
+?>
