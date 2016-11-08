67e68646526a29fe502fb1baecce4a85f7065ccb
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-08 16:37:13 +0900

    コード整理

--- register/regist_confirm.php
+++ register/regist_confirm.php
@@ -9,15 +9,4 @@
 	$userPass = $_POST['input_password'];
 	$userEmail = $_POST['input_email'];
 
-	//データ入力チェック
-	if($userId == "",$userPass == "", $userEmail == ""){
-		array_push($error, "入力データが不足");
-	}
-
-	//パスチェック.強化の必要あり
-	if(strlen($input_password) < 6 || strlen($input_password) > 16) {
-		array_push($error,"pass 6-16");
-	}
-
-
 ?>
