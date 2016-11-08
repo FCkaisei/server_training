4281757537a48bac96e9193c760dfb2ec31c4b3e
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-11-08 16:36:35 +0900

    コード整理

--- register/regist_confirm.php
+++ register/regist_confirm.php
@@ -19,27 +19,5 @@
 		array_push($error,"pass 6-16");
 	}
 
-?>
-<div>
-<?php
-	if(count($error) > 0) {
-?>
-</div>
-<?php
-	}else {
-?>
-<form method="post" action="./user_regist.php">
-	<table>
-		<caption>入力情報確認ページ</caption>
-		<tr>
-			<td class="item">ユーザー名：</td>
-			<td>
-				<?php print $userId;?><input type="hidden" name="input_userid" value="<?php print $userId;?>"></td>
-		</tr>
-		
-	</table>
-	<div><input type="submit" value=" 登 録 "></div>
-</form>
-<?php
-	}
+
 ?>
