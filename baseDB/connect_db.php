19483954ffadd9535948f148666272c4a88f4652
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-06 03:42:05 +0900

    サーバーの移行チェック

--- baseDB/connect_db.php
+++ baseDB/connect_db.php
@@ -1,7 +1,7 @@
 <?php
-	$dsn = 'mysql:dbname=UserLoginDB;host=localhost';
-	$user = "root";
-	$password = "";
+	$dsn = 'mysql:dbname=Twitter;host=localhost';
+	$user = "develop";
+	$password = "welcomMySQL";
 try{
 	$pdo = new PDO($dsn, $user, $password);
 	$pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
