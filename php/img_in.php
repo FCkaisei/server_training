05ab867e0ee12489548239d0ee732b396ce4f930
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-10 22:05:21 +0900

    同期

--- php/img_in.php
+++ php/img_in.php
@@ -2,30 +2,42 @@
 error_log("一応入りはしているよね",0);
 session_start();
 $user_id = $_SESSION['user'];
-$upfile = $_POST["pic"];
-$fi = $_FILES["pic"];
-error_log("-----あなたのIDーーー".$user_id,0);
-error_log("-----あなたのDATAーーー".$upfile,0);
-error_log("-----あなたのDATAーーー".$fi,0);
+
+if(!empty($_POST)){
+	//バイナリデータ
+	$fp = fopen($_FILES["image"]["tmp_name"], "rb");
+	$imgdat = fread($fp, filesize($_FILES["image"]["tmp_name"]));
+	fclose($fp);
+    $imgdat = addslashes($imgdat);
+
+	// 拡張子
+    $dat = pathinfo($_FILES["image"]["name"]);
+    $extension = $dat['extension'];
+
+	// MIMEタイプ
+   if ( $extension == "jpg" || $extension == "jpeg" ) $mime = "image/jpeg";
+   else if( $extension == "gif" ) $mime = "image/gif";
+   else if ( $extension == "png" ) $mime = "image/png";
+
+   // MySQL登録（改造しよう)
+   require_once("../baseDB/connect_db.php");
+
+   $stmt = $pdo -> prepare("UPDATE user_data SET user_img = :user_img, mime = :mime WHERE user_id = :user_id");
+   $stmt-> bindValue(':user_img', $imgdat, PDO::LOB);
+   $stmt-> bindValue(':mine', $mime, PDO::PARAM_STR);
+   $stmt-> bindValue(':user_id', $user_id, PDO::PARAM_STR);
 
 
-if ($fi){
-	error_log("ーーーーーーーーーー入ったよーーーーーーーーーー",0);
-	// ファイル取得
-	$imgdat = file_get_contents($fi);
-	require_once("../baseDB/connect_db.php");
-	$stmt = $pdo -> prepare("UPDATE user_data SET user_img = :user_img WHERE user_id = :user_id");
-	$stmt-> bindValue(':user_id', $user_id, PDO::PARAM_STR);
-	$stmt-> bindValue(':user_img', $imgdat, PDO::PARAM_STR);
-	$stmt->execute();
+   $stmt->execute();
 
-	if($stmt == false){
-		error_log("--------------フォローできませんでした--------------",0);
-	}
-	else{
-		error_log("----------------GOOD! follow QED-----------------",0);
-	}
-} else{
-	error_log("ーーーーーーーーーー入れてないねーーーーーーーーーー",0);
+   if($stmt == false){
+	   error_log("--------------フォローできませんでした--------------",0);
+   }
+   else{
+	   error_log("----------------GOOD! follow QED-----------------",0);
+   }
+}
+else{
+   error_log("ーーーーーーーーーー入れてないねーーーーーーーーーー",0);
 }
 	?>
