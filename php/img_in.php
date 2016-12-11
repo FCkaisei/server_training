18800b308758981e5bd427ae46783c41a224c2b8
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-11 14:27:56 +0900

    同期

--- php/img_in.php
+++ php/img_in.php
@@ -1,45 +1,47 @@
 <?php
-error_log("一応入りはしているよね",0);
+
+error_log('一応入りはしているよね', 0);
 session_start();
 $user_id = $_SESSION['user'];
 
-if(!empty($_POST)){
-	//バイナリデータ
-	$fp = fopen($_FILES["image"]["tmp_name"], "rb");
-	$imgdat = fread($fp, filesize($_FILES["image"]["tmp_name"]));
-	fclose($fp);
+if (!empty($_POST)) {
+    //バイナリデータ
+    $fp = fopen($_FILES['image']['tmp_name'], 'rb');
+    $imgdat = fread($fp, filesize($_FILES['image']['tmp_name']));
+    fclose($fp);
     $imgdat = addslashes($imgdat);
 
-	// 拡張子
-    $dat = pathinfo($_FILES["image"]["name"]);
+    // 拡張子
+    $dat = pathinfo($_FILES['image']['name']);
     $extension = $dat['extension'];
 
-	// MIMEタイプ
-   if ( $extension == "jpg" || $extension == "jpeg" ) $mime = "image/jpeg";
-   else if( $extension == "gif" ) $mime = "image/gif";
-   else if ( $extension == "png" ) $mime = "image/png";
-
-
-	  error_log("--------------".$imgdat."--------------",0);
-
-   	   error_log("-------------".$mime."--------------",0);
-	   error_log("-------------".$user_id."--------------",0);
-   // MySQL登録（改造しよう)
-   require_once("../baseDB/connect_db.php");
-   $stmt = $pdo -> prepare("UPDATE user_data SET img_blob = :user_img, mime = :mime WHERE user_id = :user_id");
-   $stmt-> bindValue(':user_img', $imgdat, PDO::PARAM_LOB);
-   $stmt-> bindValue(':mime', $mime, PDO::PARAM_STR);
-   $stmt-> bindValue(':user_id', $user_id, PDO::PARAM_STR);
-   $stmt->execute();
-
-   if($stmt == false){
-	   error_log("--------------フォローできませんでした--------------",0);
+    // MIMEタイプ
+   if ($extension == 'jpg' || $extension == 'jpeg') {
+       $mime = 'image/jpeg';
+   } elseif ($extension == 'gif') {
+       $mime = 'image/gif';
+   } elseif ($extension == 'png') {
+       $mime = 'image/png';
    }
-   else{
-	   error_log("----------------GOOD! follow QED-----------------",0);
-   }
+
+	error_log('--------------'.$imgdat.'--------------', 0);
+    error_log('-------------'.$mime.'--------------', 0);
+    error_log('-------------'.$user_id.'--------------', 0);
+
+    $img_base64 = base64_encode($imgdat);
+
+       // MySQL登録（改造しよう)
+   require_once '../baseDB/connect_db.php';
+    $stmt = $pdo->prepare('UPDATE user_data SET img_base = :user_img, mime = :mime WHERE user_id = :user_id');
+    $stmt->bindValue(':user_img', $img_base64, PDO::PARAM_STR);
+    $stmt->bindValue(':mime', $mime, PDO::PARAM_STR);
+    $stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
+    $stmt->execute();
+    if ($stmt == false) {
+        error_log('--------------フォローできませんでした--------------', 0);
+    } else {
+        error_log('----------------GOOD! follow QED-----------------', 0);
+    }
+} else {
+    error_log('ーーーーーーーーーー入れてないねーーーーーーーーーー', 0);
 }
-else{
-   error_log("ーーーーーーーーーー入れてないねーーーーーーーーーー",0);
-}
-	?>
--- php/user_search.php
+++ php/user_search.php
@@ -12,7 +12,7 @@
 	$stmt->execute();
 	$result = $stmt->fetchAll();
 
-	$result[0]["img_blob"] = base64_encode($result[0]["img_blob"]);
+	$result[0]["img_base"] = base64_encode($result[0]["img_base"]);
 	error_log('-------------------'.$result[0]["img_blob"].'--------------------',0);
 	$resultJson = json_encode($result);
 	echo $resultJson;
