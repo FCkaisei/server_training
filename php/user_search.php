dbdb8ea9cf9502a66333da3640563bf5ae584cf3
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-11 12:51:33 +0900

    同期

--- php/user_search.php
+++ php/user_search.php
@@ -14,8 +14,7 @@
 
 	error_log(var_dump($result),0);
 
-	error_log($result[0]['img_blob']);
-
+	error_log($result[0]['img_blob'],0);
 	// for ($i = 0; $i <= count($result); $i++) {
 	// 	$result[$i]["img_blob"] = string base64_encode($result[$i]["img_blob"]);
 	// }
