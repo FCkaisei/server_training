5b010765991d57a92570eaa591dc0b186f612331
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-11 13:13:53 +0900

    同期

--- php/user_search.php
+++ php/user_search.php
@@ -11,11 +11,9 @@
 
 	$stmt->execute();
 	$result = $stmt->fetchAll();
-	$result[0]["img_blob"] = base64_encode($result[0]["img_blob"]);
-		error_log($result[0]['img_blob'],0);
-	// for ($i = 0; $i <= count($result); $i++) {
-	// 	$result[$i]["img_blob"] = string base64_encode($result[$i]["img_blob"]);
-	// }
+	for ($i = 0; $i <= count($result); $i++) {
+		$result[$i]["img_blob"] = base64_encode($result[$i]["img_blob"]);
+	}
 	error_log(var_dump($result),0);
 	$resultJson = json_encode($result);
 	echo $resultJson;
