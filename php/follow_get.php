c0930be57950a80df911caa95f165d8bd5284023
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-11 12:35:59 +0900

    同期

--- php/follow_get.php
+++ php/follow_get.php
@@ -6,6 +6,12 @@
 	$stmt->bindValue(1, $userId, PDO::PARAM_STR);
 	$stmt->execute();
 	$result = $stmt->fetchAll();
+	//string base64_encode ( string $data );
+	error_log(var_dump($result),0);
+	for ($i = 0; $i <= count($result); $i++) {
+    	$result[$i][4] = string base64_encode(string $result[$i][4]);
+	}
+	error_log(var_dump($result),0);
 	$resultJson = json_encode($result);
 	echo $resultJson;
 ?>
