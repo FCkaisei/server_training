72e3593305f9a8d362e88d497bbf713447e2cbf4
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-11 17:00:05 +0900

    同期

--- php/img_in.php
+++ php/img_in.php
@@ -4,7 +4,7 @@ error_log('一応入りはしているよね', 0);
 session_start();
 $user_id = $_SESSION['user'];
 $user_image = $_FILES["image"];
-echo($user_image);
+echo(var_dump($user_image));
 if (!empty($_POST)) {
 
 	$img = base64_encode(file_get_contents($user_img));
