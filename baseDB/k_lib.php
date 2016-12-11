e8b40a337bd38fc1c186a6be9c202b33d1896db9
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-12 00:25:44 +0900

    同期

--- baseDB/k_lib.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once '../baseDB/connect_db.php';
-	$error = array();
-	function CheckError($error_data){
-		for ($i = 0 ; $i < count($error); $i++) {
-			error_log("ERROR:".$error,0);
-		}
-	}
- ?>
