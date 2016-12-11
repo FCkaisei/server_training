7a7611bb8debf510c3519bb71571c558dee71f76
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-11 17:04:51 +0900

    同期

--- php/img_in.php
+++ php/img_in.php
@@ -3,11 +3,11 @@
 error_log('一応入りはしているよね', 0);
 session_start();
 $user_id = $_SESSION['user'];
-$user_image = $_FILES["image"]["name"];
+$user_image = $_FILES["image"];
 echo(var_dump($user_image));
 if (!empty($_POST)) {
 
-	$img = base64_encode(file_get_contents($user_imge));
+	$img = base64_encode(file_get_contents($user_image));
     //バイナリデータ
     $fp     = fopen($_FILES['image']['tmp_name'], 'rb');
     $imgdat = fread($fp, filesize($_FILES['image']['tmp_name']));
