6fb2c685ec6979d0fa0cd22a94e58792cc2bae11
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-11 21:16:21 +0900

    同期

--- php/tweet_get.php
+++ php/tweet_get.php
@@ -14,7 +14,20 @@
 
 	$lowLim = $page * 4 - 4;
 	$highLim = 5;
-	$stmt = $pdo->prepare("SELECT * FROM tweet_data WHERE user_id IN (SELECT user_id FROM follow_data WHERE user_follow_id LIKE ?) ORDER BY user_tweet_time DESC LIMIT ?,?");
+
+	$stmt = $pdo->prepare("SELECT tweet_data.user_id, tweet_data.user_tweet, user_data.img_base, tweet_data.user_tweet_time
+		FROM tweet_data
+		INNER JOIN user_data
+		ON tweet_data.user_id
+		IN (
+			SELECT user_id
+			FROM follow_data
+			WHERE user_follow_id
+			LIKE ?
+		)
+		ORDER BY user_tweet_time
+		DESC LIMIT ?,?");
+
 	$stmt->bindValue(1, $tmpSess, PDO::PARAM_STR);
 	$stmt->bindValue(2, $lowLim, PDO::PARAM_INT);
 	$stmt->bindValue(3, $highLim, PDO::PARAM_INT);
