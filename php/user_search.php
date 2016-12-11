a7d1ce4ca636dabd6ec3237897f3e1280a0a2ba6
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-11 14:39:45 +0900

    同期

--- php/user_search.php
+++ php/user_search.php
@@ -11,9 +11,6 @@
 
 	$stmt->execute();
 	$result = $stmt->fetchAll();
-
-	$result[0]["img_base"] = base64_encode($result[0]["img_base"]);
-	error_log('-------------------'.$result[0]["img_blob"].'--------------------',0);
 	$resultJson = json_encode($result);
 	echo $resultJson;
 ?>
