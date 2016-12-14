1f4c8af843821d78ff4922ac18909d3e65b375bb
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-15 04:56:49 +0900

    修正

--- php/DAO.php
+++ php/DAO.php
@@ -17,6 +17,10 @@ switch($action){
 	case "getFollowUser":
 		$Dao->getFollowUser();
 	break;
+
+	case "setFollowUser":
+		$Dao->setFollowUser();
+	break;
 }
 
 	class DAO{
@@ -122,6 +126,23 @@ switch($action){
 			$result = $stmt->fetchAll();
 			$resultJson = json_encode($result);
 			echo $resultJson;
+		}
+
+		public function setFollowUser(){
+			session_start();
+			$user_id = $_SESSION['user'];
+			$follow_id = $_POST["user_id"];
+			require_once("../baseDB/connect_db.php");
+			$stmt = $pdo->prepare("INSERT INTO follow_data(user_id,user_follow_id)VALUES(:user_id,:follow_id)");
+			$stmt->bindValue(':user_id',$user_id,PDO::PARAM_STR);
+			$stmt->bindValue(':follow_id', $follow_id,PDO::PARAM_STR);
+			$stmt->execute();
+			if($stmt == false){
+				error_log("フォローできませんでした",0);
+			}
+			else{
+				error_log("GOOD! follow QED",0);
+			}
 		}
 
 	}
