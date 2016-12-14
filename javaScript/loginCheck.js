3fb6988d12d9e15db74769ae6dd113d84bb63d23
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-15 05:09:17 +0900

    修正

--- javaScript/loginCheck.js
+++ javaScript/loginCheck.js
@@ -9,8 +9,8 @@ req.onreadystatechange = function() {
 function execPost() {
 	var userIdValue = document.getElementById('userId').value;
 	var userPassValue = document.getElementById('userPass').value;
-	var param = "user_data="+userIdValue;
-	req.open('POST', '../php/login.php',true);
+	var param = "action=Login&user_data="+userIdValue;
+	req.open('POST', '../php/DAO.php',true);
 	req.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
 	req.send(param);
 }
