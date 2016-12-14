485245623982d9aeddefb02c2ac6248a8af997da
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-14 17:44:59 +0900

    全ページユーザーアイコン表示機能追加

--- php/tweet_get.php
+++ php/tweet_get.php
@@ -17,17 +17,18 @@
     $highLim = 5;
 
 
-	//ツイート呼び出し
-    //$stmt = $pdo->prepare('SELECT * FROM tweet_data WHERE user_id = ? OR user_id IN (SELECT user_follow_id FROM follow_data WHERE user_id LIKE ?) ORDER BY user_tweet_time DESC LIMIT ?,?');
+
 	$stmt = $pdo->prepare('SELECT tweet_data.user_tweet, tweet_data.user_id,user_data.img_base FROM tweet_data INNER JOIN user_data ON tweet_data.user_id = user_data.user_id WHERE tweet_data.user_id = ? OR tweet_data.user_id IN (SELECT user_follow_id FROM follow_data WHERE user_id LIKE ?) ORDER BY user_tweet_time DESC LIMIT ?,?');
 	$stmt->bindValue(1, $user_id, PDO::PARAM_STR);
 	$stmt->bindValue(2, $user_id, PDO::PARAM_STR);
     $stmt->bindValue(3, $lowLim, PDO::PARAM_INT);
     $stmt->bindValue(4, $highLim, PDO::PARAM_INT);
     $stmt->execute();
+
     $result = $stmt->fetchAll();
     $resultJson = json_encode($result);
-    if ($stmt == false) {
+
+	if ($stmt == false) {
 		array_push($error, "DBを操作できません");
     }
 	if($error){
@@ -36,6 +37,5 @@
 		}
 	}
 	else {
-        echo $resultJson;
+		echo $resultJson;
 	}
-	?>
