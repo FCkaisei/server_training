1cb856a59c5e315627d4cd1d5adac871b78c847a
zvnX06202 <c011343171@edu.teu.ac.jp>
2016-12-15 08:28:02 +0900

    修正

--- php/TweetDAO.php
+++ php/TweetDAO.php
@@ -117,39 +117,7 @@
 					}
 				}
 			}
-
-	        private function getTweet() {
-	            $page    = $this->PostData['page'];
-	            if (empty($this->s_user_id)) {
-	                array_push($this->error,'user_idが入ってません');
-	            }
-	            if ($page == '') {
-	                $page = 1;
-	            }
-	            //ページが一より小さい場合は1
-	            $page    = max($page, 1);
-	            $lowLim  = $page * 4 - 4;
-	            $highLim = 5;
-	            $stmt    = $this->pdo->prepare('SELECT tweet_data.user_tweet, tweet_data.user_id,user_data.img_base FROM tweet_data INNER JOIN user_data ON tweet_data.user_id = user_data.user_id WHERE tweet_data.user_id = ? OR tweet_data.user_id IN (SELECT user_follow_id FROM follow_data WHERE user_id LIKE ?) ORDER BY user_tweet_time DESC LIMIT ?,?');
-	            $stmt->bindValue(1, $this->s_user_id, PDO::PARAM_STR);
-	            $stmt->bindValue(2, $this->s_user_id, PDO::PARAM_STR);
-	            $stmt->bindValue(3, $lowLim, PDO::PARAM_INT);
-	            $stmt->bindValue(4, $highLim, PDO::PARAM_INT);
-	            $stmt->execute();
-	            $result     = $stmt->fetchAll();
-	            $resultJson = json_encode($result);
-	            if ($stmt == false) {
-	                array_push($this->error, 'DBを操作できません');
-	            }
-	            if ($this->error) {
-					for ($i = 0; $i < count($this->error); ++$i) {
-						error_log('ERROR:'.$this->error[$i], 0);
-					}
-	            } else {
-	                echo $resultJson;
-	            }
-	        }
-
+			
 	        private function getLimit() {
 	            $stmt = $this->pdo->prepare('SELECT COUNT(*) FROM tweet_data WHERE user_id IN (SELECT user_follow_id FROM follow_data WHERE user_id LIKE ?) ORDER BY user_tweet_time DESC');
 	            $stmt->bindValue(1, $this->s_user_id, PDO::PARAM_STR);
